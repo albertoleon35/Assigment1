8a25cca9bc7f8c26e1c4f2260a6545d25a6652ea
Alberto Leon <albertoleon325@gmail.com>
2018-08-31 15:23:35 -0700

    Created squaredSums2

--- Assignment1.playground/Contents.swift
+++ Assignment1.playground/Contents.swift
@@ -10,5 +10,20 @@ func squaredSums(numbers: [Int])  -> Int {
     return result;
 }
 
-var integers =  [1,2,0,4];
-var value = squaredSums(numbers:integers)
+func squaredSums2(numbers: [Int]) -> Int {
+    var result = 0;
+    for number in numbers {
+        if(number != 0 && (number % 2 == 0)) {
+            result += (number * number);
+        }
+    }
+    return result;
+}
+
+
+var integers =  [1,3,0,5];
+var value = squaredSums2(numbers:integers);
+
+
+//var integers2 =  [1,2,0,4];
+//var value1 = squaredSums2(numbers:integers2);
